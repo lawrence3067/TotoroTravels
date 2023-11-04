01aeec09b6201c0987162617e6a690c91064c941
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2023-11-04 15:30:39 -0700

    Working API fetch

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
+async function getCityInfo()
+{
+    let desiredCity = document.getElementById("search").value;
+    let desiredCom = document.getElementById("thing").value;
 
+    if (desiredCom != '')
+    {
+        specificSearch = getCommodity(desiredCity, desiredCom);
+    }
+    else
+    {
+        var hotels;
+        var restaurants;
+
+        hotels = getHotels(desiredCity);
+        restaurants = getRestaurants(desiredCity);
+    }
+
+}
+
+function printPictures(city)
+{
+    Object.entries(cityData).forEach(([key, value]) =>
+    {
+        console.log(`${key}: ${value}`);
+        document.getElementById("main").innerHTML += `
+            <div class="result" id="something">
+                <img src="${value.url}" />
+            </div>`
+    });
+}
+
+async function getCommodity(city, com) {
+    cityCommodity = await fetch(`http://127.0.0.1:8000/get_yelp_data?loc=${city}&term=${com}`)
+        .then(response => response.json())
+        .catch(err => console.error(err));
+        console.log(cityCommodity)
+    return cityCommodity;
+}
+
+function getHotels(city)
+{
+    return getCommodity(city, 'hotels');
+}
+
+function getRestaurants(city)
+{
+
+    return getCommodity(city, 'restaurants');
+}
