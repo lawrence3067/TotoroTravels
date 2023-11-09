f14f3c81bf8d9a0b2b2ca8bbaa00689d538506ae
Lawrence Chau <chaulawrencejh@gmail.com>
2023-11-08 16:45:41 -0800

    w

--- dataFetch.js
+++ dataFetch.js
@@ -15,10 +15,28 @@ export async function writeToHTML(desiredCity, good)
 }
 
 export async function getCommodity(city, com) {
-    var cityCommodity;
-    cityCommodity = await fetch(`http://127.0.0.1:8000/get_yelp_data?loc=${city}&term=${com}`)
-        .then(response => response.json())
-        .catch(err => console.error(err));
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const targetUrl = `https://api.yelp.com/v3/businesses/search?location=${city}&term=${com}&sort_by=best_match&limit=5`
+    //location=${city}&term=${com}&sort_by=best_match&
+    const queryUrl = proxyUrl + targetUrl;
 
-    return JSON.parse(cityCommodity);
+    cityCommodity = await $.ajax({
+        url: queryUrl,
+        method: "GET",
+        dataType: "json",
+        headers: {
+          "accept": "application/json",
+          "x-requested-with": "xmlhttprequest",
+          "Access-Control-Allow-Origin": "*",
+          "Authorization": `Bearer eh4N9M5tz4-1ss0daqIgspdjsHaUVaIc0gQc2zD1axSjU4i8aExBIs6umEODX5E5dCjo3y3yNFeM_9yKtblbN7fW94GxtOeoysF8uzStsEKHUKc38lnnKfQaAh1IZXYx`
+        }
+      })
+
+    temp = JSON.stringify(cityCommodity);
+      
+    // cityCommodity = await fetch(proxyUrl + targetUrl, options)
+    //     .then(response => response.json())
+    //     .catch(err => console.error(err));
+
+    return JSON.parse(temp);
 }
--- searched.js
+++ searched.js
@@ -45,11 +45,6 @@ async function getCommodity(city, com) {
           "Access-Control-Allow-Origin": "*",
           "Authorization": `Bearer eh4N9M5tz4-1ss0daqIgspdjsHaUVaIc0gQc2zD1axSjU4i8aExBIs6umEODX5E5dCjo3y3yNFeM_9yKtblbN7fW94GxtOeoysF8uzStsEKHUKc38lnnKfQaAh1IZXYx`
         }
-        //,
-        // data: {
-        //   term: com,
-        //   location: city
-        // }
       })
 
     temp = JSON.stringify(cityCommodity);
